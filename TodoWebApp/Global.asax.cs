240d51732345cefaf57456e661305930d331a6ba
ExiadiceJinTree <daisuke.2ki@gmail.com>
2020-05-11 15:20:06 +0900

    EntityFrameworkの自動Migration機能を有効化し、UserとRoleの初期データ投入処理実装。

--- TodoWebApp/Global.asax.cs
+++ TodoWebApp/Global.asax.cs
@@ -1,18 +1,35 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using TodoWebApp.Migrations;
+using TodoWebApp.Models;
 
 namespace TodoWebApp
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        /// <summary>
+        /// 本プロジェクトアプリケーションが起動したときに最初に呼ばれるメソッド。
+        /// </summary>
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            //****************************************************************************************************
+            // 以下、自動生成後の手動追加コード:
+            //****************************************************************************************************
+            // EntityFrameworkの初期化処理 - Migrations.Configurationクラスの実行
+            // * 以下のようにすることで、DBの自動マイグレーションを行え、Migrations.ConfigurationクラスのSeedメソッドを呼び初期データ自動投入も行える。
+            // * Database.SetInitializerメソッド: DBの初期化方法を設定する。DBの自動生成方法をカスタマイズできる。
+            //   - SetInitializerメソッドの引数: 初期化方法を示すIDatabaseInitializerインターフェイスを実装したクラスを指定。あらかじめいくつかのクラスが用意されている。
+            //   - SetInitializerメソッドにMigrateDatabaseToLatestVersionインスタンスを渡すことでDBの自動マイグレーションを行うことができる。
+            //   - 初期データを投入するには、MigrateDatabaseToLatestVersionクラスの型引数であるMigrations.ConfigurationクラスのSeedメソッドに、初期データ投入用コードを記述する。
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<TodoesContext, Configuration>());
         }
     }
 }
---
+++ TodoWebApp/Migrations/Configuration.cs
@@ -0,0 +1,81 @@
+﻿/// <summary>
+/// EntityFrameworkの自動Migration機能を有効にするための設定。
+/// NuGetパッケージマネージャーコンソールで、以下のコマンドを実行することで、Migrationsフォルダと本クラスファイルを自動生成。
+///   > Enable-Migrations -EnableAutomaticMigrations
+/// * Migration機能: Modelの変更に合わせてDBにその内容が反映(作成・変更)される仕組み。
+/// </summary>
+namespace TodoWebApp.Migrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using TodoWebApp.Models;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TodoWebApp.Models.TodoesContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;  // データ損失となるモデルの変更(列の削除等)についても自動マイグレーション(DBへの自動反映)を許可する。 * 自動生成後の手動追加設定。
+            ContextKey = "TodoWebApp.Models.TodoesContext";
+        }
+
+        protected override void Seed(TodoWebApp.Models.TodoesContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+
+            //****************************************************************************************************
+            // 以下、自動生成後の手動追加コード:
+            // ・UserとRoleの初期データを登録する処理を実装。
+            //   - Userの初期データ: 管理者, 一般ユーザー
+            //   - Roleの初期データ: Administrators(管理者), Users(一般ユーザー)
+            //****************************************************************************************************
+            User admin = new User()  // 管理者
+            {
+                Id = 1,
+                UserName = "admin",
+                Password = "password",
+                Roles = new List<Role>(),
+            };
+
+            User generalUser01 = new User()  // 一般ユーザー
+            {
+                Id = 2,
+                UserName = "GeneralUser01",
+                Password = "password",
+                Roles = new List<Role>(),
+            };
+
+            Role administrators = new Role()  // Administrators(管理者)
+            {
+                Id = 1,
+                RoleName = "Administrators",
+                Users = new List<User>(),
+            };
+
+            Role users = new Role()  // Users(一般ユーザー)
+            {
+                Id = 1,
+                RoleName = "Users",
+                Users = new List<User>(),
+            };
+
+            admin.Roles.Add(administrators);
+            administrators.Users.Add(admin);
+
+            generalUser01.Roles.Add(users);
+            users.Users.Add(generalUser01);
+
+            // UserとRoleの情報をDBに反映
+            // * 本Seedメソッドの引数のcontextパラメータを利用してDBにアクセス。
+            // *  DbSet<T>.AddOrUpdate(identifierExpression, entities)メソッド: DBに既存でなければ追加、既存なら更新。第1引数identifierExpressionに指定した値と同等のキーが存在すればAdd、存在しなければUpdate。
+            context.Users.AddOrUpdate(user => user.Id, new User[] { admin, generalUser01 });
+            context.Roles.AddOrUpdate(role => role.Id, new Role[] { administrators, users });
+        }
+    }
+}
